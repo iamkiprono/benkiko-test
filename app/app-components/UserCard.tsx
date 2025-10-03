3732a1b9ca581593942eced64f1b6ffbb034debc
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2025-10-03 13:16:46 +0300

    Update button styles to use rounded-full
    Changed the Sign In and user email buttons to use the 'rounded-full' class for fully rounded corners, improving visual consistency.

--- app/app-components/UserCard.tsx
+++ app/app-components/UserCard.tsx
@@ -9,12 +9,12 @@ const UserCard = () => {
     <div>
          {!user &&  <button 
          onClick={login}
-        className="bg-yellow-500 text-white px-8 py-4 rounded-lg text-lg font-semibold hover:bg-yellow-600 transition">
+        className="bg-yellow-500 text-white px-8 py-4 rounded-lg text-lg font-semibold hover:bg-yellow-600 transition rounded-full">
           Sign In
         </button>}
         {user &&
 
-            <button onClick={logout} className="bg-[var(--color-accent)] text-white px-8 py-4 rounded-lg text-lg font-semibold hover:bg-gray-900 transition">
+            <button onClick={logout} className="bg-[var(--color-accent)] text-white px-8 py-4 rounded-lg text-lg font-semibold hover:bg-gray-900 transition rounded-full">
            {user?.email}
         </button>
         }
